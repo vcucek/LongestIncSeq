646a2e175d3ad98f9cf8c7bfe16170df2434eeef
Vito Čuček <vito.cucek@gmail.com>
2022-02-06 23:59:43 +0100

    Add missing header documentation

--- include/SequenceAnalyser.hpp
+++ include/SequenceAnalyser.hpp
@@ -3,15 +3,31 @@
 
 #include "FloatSequence.hpp"
 
+/**
+ * Interval defining subsequence
+ * with strat (inclusive) and end (exclusive) indexes.
+ */
 typedef struct
 {
     int startIndex = -1;
     int endIndex = -1;
 } Interval;
 
+/**
+ * Common interface class for processing.
+ */
 class SequenceAnalyser
 {
 public:
+
+    /**
+     * Caclulate longest continuous interval where values in the provided sequence strictly increase.
+     * The returned interval contains start (inclusive) and end (exclusive) indexes.
+     * Consequently {0,1} is an empty interval returned when providing sequence with zero size.
+     *
+     * @param seq is a sequnce of float values.
+     * @return interval defining subsequence with strictly increasing values.
+     */
     virtual Interval calcLongestIncInterval(FloatSequence &seq) = 0;
 
     static bool intervalEquals(Interval &a, Interval &b)
@@ -24,6 +40,9 @@ public:
     }
 };
 
+/**
+ * CUDA implementation of the SequenceAnalyser.
+ */
 class SequenceAnalyserCUDA : public SequenceAnalyser
 {
 public:
@@ -31,6 +50,9 @@ public:
     virtual Interval calcLongestIncInterval(FloatSequence &seq);
 };
 
+/**
+ * Basic CPU implementation of the SequenceAnalyser.
+ */
 class SequenceAnalyserCPU : public SequenceAnalyser
 {
 public:
